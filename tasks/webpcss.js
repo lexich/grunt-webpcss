ad0eb0b9f382b0564240c64368094388af8fde20
Alexey Efremov <alexey.efremov@saltpepper.ru>
2014-09-26 09:11:05 +0400

    extract functionality to webpcssp-transform

--- tasks/webpcss.js
+++ tasks/webpcss.js
@@ -9,78 +9,42 @@
 'use strict';
 
 module.exports = function(grunt) {
-  var postcss = require('postcss');
-  var _ = require('lodash');
-  
-  
-  grunt.registerMultiTask('webpcss', 'Process css file to generate addition css ruless to add webp compatble"', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      baseClass:'.webp',
-      replace_from:/\.(png|jpg|jpeg)/,
-      replace_to:'.webp'
-    });
 
-    var proc = postcss(function(css){      
-      var nodes = [];
-      css.eachDecl(function(rule, data) {
-        if(rule.prop.match(/^background/) && rule._value.match(/url/) ){
-          var selector = "";
-          var selectors = rule.parent.selector.split(',');
-          for(var index in selectors){
-            if(!!selector){
-              selector += ", ";
-            }
-            selector += options.baseClass + " " + selectors[index].trim();
-          }
-          var rx = options.replace_from;
-          if(!_.isRegExp(options.replace_from)){
-            rx = new RegExp(rx);
-          }
-          
-          var value = rule._value.replace(rx,options.replace_to);
-          var prop = rule.prop;
-          if( value.indexOf(",") === -1 && /url[ ]*\((.+)\)/g.exec(value) ){
-            value = "url(" + RegExp.$1 + ");";
-            prop = "background-image";
-          }
-          var new_rule = postcss.rule({selector:selector});
-          new_rule.append({
-            prop:prop,
-            value: value
-          });
-          nodes.push(new_rule);
-        }
-      });
-      for(var n=0; n < nodes.length; n++){
-        css.append(nodes[n]);
-      }
-    });
+	var _ = require('lodash');
+	var webpcss_transform = require('../lib/webpcss-transform');
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        var data = grunt.file.read(filepath);
-        return proc.process(data).css;
-      }).join('\n');
+	grunt.registerMultiTask('webpcss', 'Process css file to generate addition css ruless to add webp compatble"', function() {
+		// Merge task-specific and/or target-specific options with these defaults.
+		var options = this.options({
+			baseClass:'.webp',
+			replace_from:/\.(png|jpg|jpeg)/,
+			replace_to:'.webp'
+		});
+
+		// Iterate over all specified file groups.
+		this.files.forEach(function(f) {
+			// Concat specified files.
+			var src = f.src.filter(function(filepath) {
+				// Warn on and remove invalid source files (if nonull was set).
+				if (!grunt.file.exists(filepath)) {
+					grunt.log.warn('Source file "' + filepath + '" not found.');
+					return false;
+				} else {
+					return true;
+				}
+			}).map(function(filepath) {
+				// Read file source.
+				var data = grunt.file.read(filepath);
+				return webpcss_transform(data, options);
+			}).join('\n');
 
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+			// Write the destination file.
+			grunt.file.write(f.dest, src);
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
+			// Print a success message.
+			grunt.log.writeln('File "' + f.dest + '" created.');
+		});
+	});
 
 };
