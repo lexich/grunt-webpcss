21f307e65ae18b79aea02fcb5eaa73a4726cc19c
Efremov Alexey <lexich121@gmail.com>
2015-05-05 10:27:46 +0300

    Add: code style checker

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,26 +6,25 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
+"use strict";
 
 module.exports = function(grunt) {
-
   // Project configuration.
   grunt.initConfig({
     jshint: {
       all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
+        "Gruntfile.js",
+        "tasks/*.js",
+        "<%= nodeunit.tests %>"
       ],
       options: {
-        jshintrc: '.jshintrc',
-      },
+        jshintrc: ".jshintrc"
+      }
     },
 
     // Before generating any new files, remove any previously-created files.
     clean: {
-      tests: ['tmp'],
+      tests: ["tmp"]
     },
 
     // Configuration to be run (and then tested).
@@ -34,40 +33,38 @@ module.exports = function(grunt) {
         options: {
         },
         files: {
-          'tmp/default_options.css': ['test/fixtures/testing.css'],
-        },
+          "tmp/default_options.css": ["test/fixtures/testing.css"]
+        }
       },
       custom_options: {
         options: {
-          baseClass:'.webp1',          
-          replace_to:'.webp'
+          baseClass:".webp1",
+          replace_to:".webp"
         },
         files: {
-          'tmp/custom_options.css': ['test/fixtures/testing.css'],
-        },
-      },
+          "tmp/custom_options.css": ["test/fixtures/testing.css"]
+        }
+      }
     },
 
     // Unit tests.
     nodeunit: {
-      tests: ['test/*_test.js'],
-    },
-
+      tests: ["test/*_test.js"]
+    }
   });
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+  // Actually load this plugin"s task(s).
+  grunt.loadTasks("tasks");
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-nodeunit");
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'webpcss', 'nodeunit', 'clean']);
+  // plugin"s task(s), then test the result.
+  grunt.registerTask("test", ["clean", "webpcss", "nodeunit", "clean"]);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
-
+  grunt.registerTask("default", ["jshint", "test"]);
 };
--- tasks/webpcss.js
+++ tasks/webpcss.js
@@ -1,54 +1,52 @@
+"use strict";
+
 /*
  * grunt-webpcss
  * https://github.com/lexich/grunt-webpcss
  *
- * Copyright (c) 2014 Efremov Alexey
+ * Copyright (c) 2015 Efremov Alexey
  * Licensed under the MIT license.
  */
-
-'use strict';
+var _ = require("lodash"),
+    webpcss = require("webpcss");
 
 module.exports = function(grunt) {
+  grunt.registerMultiTask("webpcss", "Process css file to generate addition css ruless to add webp compatble", function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      baseClass:".webp",
+      replace_from:/\.(png|jpg|jpeg)/,
+      replace_to:".webp"
+    });
 
-	var _ = require('lodash');
-	var webpcss = require('webpcss');
+    var done = _.after(this.files.length, this.async());
 
-	grunt.registerMultiTask('webpcss', 'Process css file to generate addition css ruless to add webp compatble"', function() {
-		// Merge task-specific and/or target-specific options with these defaults.
-		var options = this.options({
-			baseClass:'.webp',
-			replace_from:/\.(png|jpg|jpeg)/,
-			replace_to:'.webp'
-		});
-		
-		var done = _.after(this.files.length, this.async());
-		// Iterate over all specified file groups.
-		this.files.forEach(function(f) {
-			// Concat specified files.
-			var src = f.src.filter(function(filepath) {
-				// Warn on and remove invalid source files (if nonull was set).
-				if (!grunt.file.exists(filepath)) {
-					grunt.log.warn('Source file "' + filepath + '" not found.');
-					return false;
-				} else {
-					return true;
-				}
-			}).map(function(filepath) {
-				// Read file source.
-				return grunt.file.read(filepath);				
-			}).join('\n');
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn("Source file \"" + filepath + "\" not found.");
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join("\n");
 
+      webpcss.transform(src, options)
+        .then(function(res) {
+          // Write the destination file.
+          grunt.file.write(f.dest, res);
 
-			webpcss.transform(src, options)
-				.then(function(res) {
-					// Write the destination file.
-					grunt.file.write(f.dest, res);		
-					// Print a success message.
-					grunt.log.writeln('File "' + f.dest + '" created.');
-					done();
-				})
-				.catch(done);
-		});
-	});
-
+          // Print a success message.
+          grunt.log.writeln("File \"" + f.dest + "\" created.");
+          done();
+        })
+        .catch(done);
+    });
+  });
 };
